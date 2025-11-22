17681b4db0bfa0e611cab1a774fdf456d1f90da6
KamilMarszalek <kamilmar553@gmail.com>
2025-11-23 00:23:06 +0100

    feat: implement channel management service

---
+++ internal/domain/channels/service.go
@@ -0,0 +1,74 @@
+package channels
+
+import (
+	"context"
+
+	msmodels "github.com/microsoftgraph/msgraph-sdk-go/models"
+)
+
+type Service struct {
+	api ChannelAPI
+}
+
+func NewService(api ChannelAPI) *Service {
+	return &Service{api: api}
+}
+
+func (s *Service) ListChannels(ctx context.Context, teamID string) ([]*Channel, error) {
+	resp, err := s.api.ListChannels(ctx, teamID)
+	if err != nil {
+		return nil, mapError(err)
+	}
+	var channels []*Channel
+	for _, ch := range resp.GetValue() {
+		channels = append(channels, &Channel{
+			ID:        deref(ch.GetId()),
+			Name:      deref(ch.GetDisplayName()),
+			IsGeneral: deref(ch.GetDisplayName()) == "General",
+		})
+	}
+	return channels, nil
+}
+
+func (s *Service) Get(ctx context.Context, teamID, channelID string) (*Channel, error) {
+	resp, err := s.api.GetChannel(ctx, teamID, channelID)
+	if err != nil {
+		return nil, mapError(err)
+	}
+	return &Channel{
+		ID:        deref(resp.GetId()),
+		Name:      deref(resp.GetDisplayName()),
+		IsGeneral: deref(resp.GetDisplayName()) == "General",
+	}, nil
+}
+
+func (s *Service) Create(ctx context.Context, teamID, name string) (*Channel, error) {
+	newChannel := msmodels.NewChannel()
+	newChannel.SetDisplayName(&name)
+
+	created, err := s.api.CreateChannel(ctx, teamID, newChannel)
+	if err != nil {
+		return nil, mapError(err)
+	}
+	return &Channel{
+		ID:        deref(created.GetId()),
+		Name:      deref(created.GetDisplayName()),
+		IsGeneral: false,
+	}, nil
+}
+
+func (s *Service) Delete(ctx context.Context, teamID, channelID string) error {
+	err := s.api.DeleteChannel(ctx, teamID, channelID)
+	if err != nil {
+		return mapError(err)
+	}
+	return nil
+}
+
+
+func deref(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
+}
