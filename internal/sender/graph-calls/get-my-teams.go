c36e089bd22b4adf4b50d7c72a6c2b46d0c9a849
Michal <01187288@pw.edu.pl>
2025-11-22 20:31:26 +0100

    fix: fixed linter issues

--- internal/sender/graph-calls/get-my-teams.go
+++ internal/sender/graph-calls/get-my-teams.go
@@ -2,20 +2,22 @@ package graphcalls
 
 import (
 	"context"
+
 	sender "github.com/pzsp-teams/lib/internal/sender"
 
 	msgraph "github.com/microsoftgraph/msgraph-sdk-go"
 	graphmodels "github.com/microsoftgraph/msgraph-sdk-go/models"
 )
 
+// GetMyTeams will be used later by other packages
 func GetMyTeams(client *msgraph.GraphServiceClient) sender.GraphCall {
-	return func(ctx context.Context) (*sender.Response, error) {
+	return func(ctx context.Context) (sender.Response, error) {
 		received, err := client.Me().JoinedTeams().Get(ctx, nil)
 		if err != nil {
 			return nil, err
 		}
 
-		response, err := ParseGetMyTeamsResponse(received)
+		response, err := parseGetMyTeamsResponse(received)
 		if err != nil {
 			return nil, err
 		}
@@ -23,7 +25,8 @@ func GetMyTeams(client *msgraph.GraphServiceClient) sender.GraphCall {
 	}
 }
 
-func ParseGetMyTeamsResponse(received graphmodels.TeamCollectionResponseable) (*sender.Response, error) {
+// nolint
+func parseGetMyTeamsResponse(received graphmodels.TeamCollectionResponseable) (sender.Response, error) {
 	// TODO implement parsing logic
 	return nil, nil
 }
