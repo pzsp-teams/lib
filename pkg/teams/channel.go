5cfb58dce0649f8be4b0c9bd03c1913e02fc928b
KamilMarszalek <kamilmar553@gmail.com>
2025-11-23 12:51:19 +0100

    fix: add comments

--- pkg/teams/channel.go
+++ pkg/teams/channel.go
@@ -6,8 +6,10 @@ import (
 	"github.com/pzsp-teams/lib/internal/domain/channels"
 )
 
+// Channel will be used later
 type Channel = channels.Channel
 
+// ChannelService will be used later
 type ChannelService interface {
 	List(ctx context.Context, teamID string) ([]*Channel, error)
 	Get(ctx context.Context, teamID, channelID string) (*Channel, error)
@@ -19,18 +21,22 @@ type channelService struct {
 	inner *channels.Service
 }
 
+// List will be used later
 func (s *channelService) List(ctx context.Context, teamID string) ([]*Channel, error) {
 	return s.inner.ListChannels(ctx, teamID)
 }
 
-func (s channelService) Get(ctx context.Context, teamID, channelID string) (*Channel, error) {
+// Get will be used later
+func (s *channelService) Get(ctx context.Context, teamID, channelID string) (*Channel, error) {
 	return s.inner.Get(ctx, teamID, channelID)
 }
 
+// Create will be used later
 func (s *channelService) Create(ctx context.Context, teamID, name string) (*Channel, error) {
 	return s.inner.Create(ctx, teamID, name)
 }
 
+// Delete will be used later
 func (s *channelService) Delete(ctx context.Context, teamID, channelID string) error {
 	return s.inner.Delete(ctx, teamID, channelID)
 }
