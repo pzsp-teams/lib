38fa7c9689f8677ecd9cf7e0f30baf74c113636c
Damian D'Souza <dsouzadamian189@gmail.com>
2025-11-28 01:28:33 +0100

    feat: implement message functions for channel service

--- pkg/teams/channels/model.go
+++ pkg/teams/channels/model.go
@@ -1,8 +1,37 @@
 package channels
 
+import "time"
+
 // Channel represents a Microsoft Teams channel
 type Channel struct {
 	ID        string
 	Name      string
 	IsGeneral bool
+}
+
+// Message represents a chat message in a Teams channel
+type Message struct {
+	ID              string
+	Content         string
+	ContentType     string
+	CreatedDateTime time.Time
+	From            *MessageFrom
+	ReplyCount      int
+}
+
+// MessageFrom represents the sender of a message
+type MessageFrom struct {
+	UserID      string
+	DisplayName string
+}
+
+// MessageBody represents the request body for sending a message
+type MessageBody struct {
+	Content string
+}
+
+// ListMessagesOptions contains options for listing messages
+type ListMessagesOptions struct {
+	Top           *int32
+	ExpandReplies bool
 }
