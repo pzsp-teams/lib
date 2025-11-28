47740da68e195d58c176388df379ef4dfd2dc909
Damian D'Souza <dsouzadamian189@gmail.com>
2025-11-28 01:28:33 +0100

    feat: implement message functions for channel service

--- pkg/teams/channels/service.go
+++ pkg/teams/channels/service.go
@@ -73,6 +73,112 @@ func (s *Service) Delete(ctx context.Context, teamID, channelID string) error {
 	return nil
 }
 
+// SendMessage will be used later
+func (s *Service) SendMessage(ctx context.Context, teamID, channelID string, body MessageBody) (*Message, error) {
+	message := msmodels.NewChatMessage()
+	messageBody := msmodels.NewItemBody()
+	messageBody.SetContent(&body.Content)
+	message.SetBody(messageBody)
+
+	resp, err := s.api.SendMessage(ctx, teamID, channelID, message)
+	if err != nil {
+		return nil, mapError(err)
+	}
+
+	return mapChatMessageToMessage(resp), nil
+}
+
+// ListMessages retrieves messages from a channel
+func (s *Service) ListMessages(ctx context.Context, teamID, channelID string, opts *ListMessagesOptions) ([]*Message, error) {
+	var top *int32
+	if opts != nil && opts.Top != nil {
+		top = opts.Top
+	}
+
+	resp, err := s.api.ListMessages(ctx, teamID, channelID, top)
+	if err != nil {
+		return nil, mapError(err)
+	}
+
+	var messages []*Message
+	for _, msg := range resp.GetValue() {
+		messages = append(messages, mapChatMessageToMessage(msg))
+	}
+
+	return messages, nil
+}
+
+// GetMessage retrieves a specific message from a channel
+func (s *Service) GetMessage(ctx context.Context, teamID, channelID, messageID string) (*Message, error) {
+	resp, err := s.api.GetMessage(ctx, teamID, channelID, messageID)
+	if err != nil {
+		return nil, mapError(err)
+	}
+
+	return mapChatMessageToMessage(resp), nil
+}
+
+// ListReplies retrieves replies to a specific message
+func (s *Service) ListReplies(ctx context.Context, teamID, channelID, messageID string, top *int32) ([]*Message, error) {
+	resp, err := s.api.ListReplies(ctx, teamID, channelID, messageID, top)
+	if err != nil {
+		return nil, mapError(err)
+	}
+
+	var replies []*Message
+	for _, reply := range resp.GetValue() {
+		replies = append(replies, mapChatMessageToMessage(reply))
+	}
+
+	return replies, nil
+}
+
+// GetReply retrieves a specific reply to a message
+func (s *Service) GetReply(ctx context.Context, teamID, channelID, messageID, replyID string) (*Message, error) {
+	resp, err := s.api.GetReply(ctx, teamID, channelID, messageID, replyID)
+	if err != nil {
+		return nil, mapError(err)
+	}
+
+	return mapChatMessageToMessage(resp), nil
+}
+
+func mapChatMessageToMessage(msg msmodels.ChatMessageable) *Message {
+	if msg == nil {
+		return nil
+	}
+
+	message := &Message{
+		ID: deref(msg.GetId()),
+	}
+
+	if body := msg.GetBody(); body != nil {
+		message.Content = deref(body.GetContent())
+		if contentType := body.GetContentType(); contentType != nil {
+			message.ContentType = contentType.String()
+		}
+	}
+
+	if created := msg.GetCreatedDateTime(); created != nil {
+		message.CreatedDateTime = *created
+	}
+
+	if from := msg.GetFrom(); from != nil {
+		if user := from.GetUser(); user != nil {
+			message.From = &MessageFrom{
+				UserID:      deref(user.GetId()),
+				DisplayName: deref(user.GetDisplayName()),
+			}
+		}
+	}
+
+	if replies := msg.GetReplies(); replies != nil {
+		message.ReplyCount = len(replies)
+	}
+
+	return message
+}
+
 func deref(s *string) string {
 	if s == nil {
 		return ""
