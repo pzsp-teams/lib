1dd1478a6397826cb96ea30273c01c8be805ab0d
KamilMarszalek <kamilmar553@gmail.com>
2025-12-03 19:12:02 +0100

    feat: add team management commands to CLI

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	msmodels "github.com/microsoftgraph/msgraph-sdk-go/models"
 
 	"github.com/pzsp-teams/lib/pkg/teams"
 	channelsPkg "github.com/pzsp-teams/lib/pkg/teams/channels"
@@ -11,7 +15,7 @@ import (
 
 func printUsage() {
 	fmt.Println("Usage: teams <command> [arguments]")
-	fmt.Println("\nAvailable commands:")
+	fmt.Println("\nChannel commands:")
 	fmt.Println("  create-channel <team-id> <channel-name>")
 	fmt.Println("  list-channels <team-id>")
 	fmt.Println("  get-channel <team-id> <channel-id>")
@@ -19,6 +23,16 @@ func printUsage() {
 	fmt.Println("  send-message <team-id> <channel-name> <message>")
 	fmt.Println("  list-messages <team-id> <channel-name> [top]")
 	fmt.Println("  list-replies <team-id> <channel-name> <message-id> [top]")
+	fmt.Println("\nTeam commands:")
+	fmt.Println("  list-my-teams")
+	fmt.Println("  get-team <team-id>")
+	fmt.Println("  create-team <display-name> <mail-nickname> <visibility>")
+	fmt.Println("  create-team-from-template <display-name> <description...>")
+	fmt.Println("  update-team <team-id> <new-display-name> [new-description...]")
+	fmt.Println("  archive-team <team-id> [spo-readonly=true|false]")
+	fmt.Println("  unarchive-team <team-id>")
+	fmt.Println("  delete-team <team-id>")
+	fmt.Println("  restore-team <deleted-group-id>")
 }
 
 func main() {
@@ -28,8 +42,8 @@ func main() {
 	}
 	cmd := os.Args[1]
 	authConfig := loadAuthConfig()
-
 	senderConfig := newSenderConfig()
+
 	client, err := teams.NewClient(context.TODO(), authConfig, senderConfig)
 	if err != nil {
 		fmt.Printf("Error creating Teams client: %v\n", err)
@@ -79,6 +93,57 @@ func main() {
 			os.Exit(1)
 		}
 		handleListReplies(client, os.Args[2:])
+	case "list-my-teams":
+		handleListMyTeams(client)
+	case "get-team":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: teams get-team <team-id>")
+			os.Exit(1)
+		}
+		handleGetTeam(client, os.Args[2:])
+	case "create-team":
+		if len(os.Args) < 5 {
+			fmt.Println("Usage: teams create-team <display-name> <mail-nickname> <visibility>")
+			os.Exit(1)
+		}
+		handleCreateTeam(client, os.Args[2:])
+	case "create-team-from-template":
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: teams create-team-from-template <display-name> <description...>")
+			os.Exit(1)
+		}
+		handleCreateTeamFromTemplate(client, os.Args[2:])
+	case "update-team":
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: teams update-team <team-id> <new-display-name> [new-description...]")
+			os.Exit(1)
+		}
+		handleUpdateTeam(client, os.Args[2:])
+	case "archive-team":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: teams archive-team <team-id> [spo-readonly=true|false]")
+			os.Exit(1)
+		}
+		handleArchiveTeam(client, os.Args[2:])
+	case "unarchive-team":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: teams unarchive-team <team-id>")
+			os.Exit(1)
+		}
+		handleUnarchiveTeam(client, os.Args[2:])
+	case "delete-team":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: teams delete-team <team-id>")
+			os.Exit(1)
+		}
+		handleDeleteTeam(client, os.Args[2:])
+	case "restore-team":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: teams restore-team <deleted-group-id>")
+			os.Exit(1)
+		}
+		handleRestoreTeam(client, os.Args[2:])
+
 	default:
 		fmt.Println("Unknown command:", cmd)
 		fmt.Println()
@@ -88,10 +153,6 @@ func main() {
 }
 
 func handleCreateChannel(client *teams.Client, args []string) {
-	if len(args) < 2 {
-		fmt.Println("Usage: teams create-channel <team-id> <channel-name>")
-		os.Exit(1)
-	}
 	teamID := args[0]
 	channelName := args[1]
 	channel, err := client.Channels.Create(context.TODO(), teamID, channelName)
@@ -103,10 +164,6 @@ func handleCreateChannel(client *teams.Client, args []string) {
 }
 
 func handleListChannels(client *teams.Client, args []string) {
-	if len(args) < 1 {
-		fmt.Println("Usage: teams list-channels <team-id>")
-		os.Exit(1)
-	}
 	teamID := args[0]
 	channels, err := client.Channels.ListChannels(context.TODO(), teamID)
 	if err != nil {
@@ -120,10 +177,6 @@ func handleListChannels(client *teams.Client, args []string) {
 }
 
 func handleGetChannel(client *teams.Client, args []string) {
-	if len(args) < 2 {
-		fmt.Println("Usage: teams get-channel <team-id> <channel-id>")
-		os.Exit(1)
-	}
 	teamID := args[0]
 	channelID := args[1]
 	channel, err := client.Channels.Get(context.TODO(), teamID, channelID)
@@ -135,10 +188,6 @@ func handleGetChannel(client *teams.Client, args []string) {
 }
 
 func handleDeleteChannel(client *teams.Client, args []string) {
-	if len(args) < 2 {
-		fmt.Println("Usage: teams delete-channel <team-id> <channel-id>")
-		os.Exit(1)
-	}
 	teamID := args[0]
 	channelID := args[1]
 	err := client.Channels.Delete(context.TODO(), teamID, channelID)
@@ -150,10 +199,6 @@ func handleDeleteChannel(client *teams.Client, args []string) {
 }
 
 func handleSendMessage(client *teams.Client, args []string) {
-	if len(args) < 3 {
-		fmt.Println("Usage: teams send-message <team-id> <channel-name> <message>")
-		os.Exit(1)
-	}
 	teamID := args[0]
 	channelName := args[1]
 	messageContent := args[2]
@@ -193,10 +238,6 @@ func handleSendMessage(client *teams.Client, args []string) {
 }
 
 func handleListMessages(client *teams.Client, args []string) {
-	if len(args) < 2 {
-		fmt.Println("Usage: teams list-messages <team-id> <channel-name> [top]")
-		os.Exit(1)
-	}
 	teamID := args[0]
 	channelName := args[1]
 
@@ -253,10 +294,6 @@ func handleListMessages(client *teams.Client, args []string) {
 }
 
 func handleListReplies(client *teams.Client, args []string) {
-	if len(args) < 3 {
-		fmt.Println("Usage: teams list-replies <team-id> <channel-name> <message-id> [top]")
-		os.Exit(1)
-	}
 	teamID := args[0]
 	channelName := args[1]
 	messageID := args[2]
@@ -318,4 +355,145 @@ func getMessageFrom(msg *channelsPkg.Message) string {
 		return msg.From.UserID
 	}
 	return "Unknown"
+}
+
+func handleListMyTeams(client *teams.Client) {
+	ctx := context.TODO()
+	ts, err := client.Teams.ListMyJoined(ctx)
+	if err != nil {
+		fmt.Printf("Error listing teams: %v\n", err)
+		os.Exit(1)
+	}
+	if len(ts) == 0 {
+		fmt.Println("You are not a member of any teams.")
+		return
+	}
+
+	fmt.Println("Teams:")
+	for _, t := range ts {
+		state := ""
+		if t.IsArchived {
+			state = " (archived)"
+		}
+		fmt.Printf("- %s (ID: %s)%s\n", t.DisplayName, t.ID, state)
+	}
+}
+
+func handleGetTeam(client *teams.Client, args []string) {
+	teamID := args[0]
+	ctx := context.TODO()
+	t, err := client.Teams.Get(ctx, teamID)
+	if err != nil {
+		fmt.Printf("Error getting team: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Team ID: %s\nName: %s\nDescription: %s\nVisibility: %s\nArchived: %v\n",
+		t.ID, t.DisplayName, t.Description, t.Visibility, t.IsArchived)
+}
+
+func handleCreateTeam(client *teams.Client, args []string) {
+	displayName := args[0]
+	mailNickname := args[1]
+	visibility := args[2] 
+
+	ctx := context.TODO()
+	t, err := client.Teams.CreateViaGroup(ctx, displayName, mailNickname, visibility)
+	if err != nil {
+		fmt.Printf("Error creating team: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Team created: %s (ID: %s)\n", t.DisplayName, t.ID)
+}
+
+func handleCreateTeamFromTemplate(client *teams.Client, args []string) {
+	displayName := args[0]
+	description := strings.Join(args[1:], " ")
+
+	ctx := context.TODO()
+	id, err := client.Teams.CreateFromTemplate(ctx, displayName, description, nil)
+	if err != nil {
+		fmt.Printf("Error creating team from template: %v\n", err)
+		os.Exit(1)
+	}
+	if id == "" {
+		fmt.Println("Team creation started (async). It may take some time before the team is available.")
+	} else {
+		fmt.Printf("Team creation started. Team (group) ID: %s\n", id)
+	}
+}
+
+func handleUpdateTeam(client *teams.Client, args []string) {
+	teamID := args[0]
+	newName := args[1]
+
+	var newDesc *string
+	if len(args) > 2 {
+		desc := strings.Join(args[2:], " ")
+		newDesc = &desc
+	}
+
+	patch := msmodels.NewTeam()
+	patch.SetDisplayName(&newName)
+	if newDesc != nil {
+		patch.SetDescription(newDesc)
+	}
+
+	ctx := context.TODO()
+	t, err := client.Teams.Update(ctx, teamID, patch)
+	if err != nil {
+		fmt.Printf("Error updating team: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Team updated: %s (ID: %s)\n", t.DisplayName, t.ID)
+}
+
+func handleArchiveTeam(client *teams.Client, args []string) {
+	teamID := args[0]
+	var spo *bool
+	if len(args) > 1 {
+		val, err := strconv.ParseBool(args[1])
+		if err != nil {
+			fmt.Printf("Error: invalid spo-readonly value (expected true/false): %v\n", err)
+			os.Exit(1)
+		}
+		spo = &val
+	}
+
+	ctx := context.TODO()
+	if err := client.Teams.Archive(ctx, teamID, spo); err != nil {
+		fmt.Printf("Error archiving team: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Team archived.")
+}
+
+func handleUnarchiveTeam(client *teams.Client, args []string) {
+	teamID := args[0]
+	ctx := context.TODO()
+	if err := client.Teams.Unarchive(ctx, teamID); err != nil {
+		fmt.Printf("Error unarchiving team: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Team unarchived.")
+}
+
+func handleDeleteTeam(client *teams.Client, args []string) {
+	teamID := args[0]
+	ctx := context.TODO()
+	if err := client.Teams.Delete(ctx, teamID); err != nil {
+		fmt.Printf("Error deleting team: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Team delete request sent (soft-delete in M365 Group).")
+}
+
+func handleRestoreTeam(client *teams.Client, args []string) {
+	deletedGroupID := args[0]
+	ctx := context.TODO()
+	id, err := client.Teams.RestoreDeleted(ctx, deletedGroupID)
+	if err != nil {
+		fmt.Printf("Error restoring team: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Team restored. Directory object ID: %s\n", id)
 }
