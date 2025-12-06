4b1de9f42b9d71b9b48e07a446c48de24919bf50
KamilMarszalek <kamilmar553@gmail.com>
2025-12-06 13:57:05 +0100

    feat: implement MessageContentType for message handling

--- channels/model.go
+++ channels/model.go
@@ -1,6 +1,17 @@
 package channels
 
-import "time"
+import (
+	"time"
+
+	msmodels "github.com/microsoftgraph/msgraph-sdk-go/models"
+)
+
+type MessageContentType string
+
+const (
+	MessageContentTypeText MessageContentType = "text"
+	MessageContentTypeHTML MessageContentType = "html"
+)
 
 // Channel represents a Microsoft Teams channel
 type Channel struct {
@@ -13,7 +24,7 @@ type Channel struct {
 type Message struct {
 	ID              string
 	Content         string
-	ContentType     string
+	ContentType     MessageContentType
 	CreatedDateTime time.Time
 	From            *MessageFrom
 	ReplyCount      int
@@ -27,7 +38,19 @@ type MessageFrom struct {
 
 // MessageBody represents the request body for sending a message
 type MessageBody struct {
-	Content string
+	Content     string
+	ContentType MessageContentType
+}
+
+func (b MessageBody) ToGraphItemBody() msmodels.ItemBodyable {
+	body := msmodels.NewItemBody()
+	body.SetContent(&b.Content)
+	ct := msmodels.TEXT_BODYTYPE
+	if b.ContentType == MessageContentTypeHTML {
+		ct = msmodels.HTML_BODYTYPE
+	}
+	body.SetContentType(&ct)
+	return body
 }
 
 // ListMessagesOptions contains options for listing messages
