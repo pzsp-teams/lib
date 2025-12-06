c244f6bbe092448a781251abf590bdec136d673c
KamilMarszalek <kamilmar553@gmail.com>
2025-12-06 19:18:21 +0100

    feat: update Cacher interface to include error handling

--- cacher/cacher_interface.go
+++ cacher/cacher_interface.go
@@ -1,8 +1,8 @@
 package cacher
 
 type Cacher interface {
-	Get(key string) any
-	Set(key string, value any)
-	Invalidate(key string)
-	Clear()
+	Get(key string) (any, bool, error)
+	Set(key string, value any) error
+	Invalidate(key string) error
+	Clear() error
 }
