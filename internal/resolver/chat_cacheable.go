0806e64371869526a3e89af8841e699b68bec1b4
Michał Szwejk <mszwejk1@gmail.com>
2025-12-29 18:30:32 +0100

    feat: implement group-chat resolver

--- internal/resolver/chat_cacheable.go
+++ internal/resolver/chat_cacheable.go
@@ -65,7 +65,37 @@ func (m *ChatResolverCacheable) ResolveOneOnOneChatRefToID(ctx context.Context, 
 }
 
 func (m *ChatResolverCacheable) ResolveGroupChatRefToID(ctx context.Context, chatRef string) (string, error) {
-	return "", nil
+	ref := strings.TrimSpace(chatRef)
+	if ref == "" {
+		return "", fmt.Errorf("empty group chat ref")
+	}
+
+	if m.cacheEnabled && m.cacher != nil {
+		key := cacher.NewGroupChatKey(ref)
+		value, found, err := m.cacher.Get(key)
+		if err == nil && found {
+			if ids, ok := value.([]string); ok && len(ids) == 1 && ids[0] != "" {
+				return ids[0], nil
+			}
+		}
+	}
+
+	chats, apiErr := m.chatsAPI.ListChats(ctx, "group")
+	if apiErr != nil {
+		return "", apiErr
+	}
+
+	idResolved, err := m.resolveGroupChatIDByTopic(ref, chats)
+	if err != nil {
+		return "", err
+	}
+
+	if m.cacheEnabled && m.cacher != nil {
+		key := cacher.NewGroupChatKey(ref)
+		_ = m.cacher.Set(key, idResolved)
+	}
+
+	return idResolved, nil
 }
 
 func (m *ChatResolverCacheable) resolveOneOnOneChatIDByUserRef(userRef string, chats msmodels.ChatCollectionResponseable) (string, error) {
@@ -89,4 +119,34 @@ func (m *ChatResolverCacheable) resolveOneOnOneChatIDByUserRef(userRef string, c
 		}
 	}
 	return "", fmt.Errorf("chat with given user %q not found", userRef)
+}
+
+func (m *ChatResolverCacheable) resolveGroupChatIDByTopic(topic string, chats msmodels.ChatCollectionResponseable) (string, error) {
+	if chats == nil || chats.GetValue() == nil || len(chats.GetValue()) == 0 {
+		return "", fmt.Errorf("no group chats avaliable")
+	}
+
+	matches := make([]msmodels.Chatable, 0, len(chats.GetValue()))
+	for _, chat := range chats.GetValue() {
+		if chat == nil {
+			continue
+		}
+		if util.Deref(chat.GetTopic()) == topic {
+			matches = append(matches, chat)
+		}
+	}
+	switch len(matches) {
+	case 0:
+		return "", fmt.Errorf("chat with given topic %q not found", topic)
+	case 1:
+		return util.Deref(matches[0].GetId()), nil
+	default:
+		var options []string
+		for _, c := range matches {
+			options = append(options,
+				fmt.Sprintf("%s (ID: %s)", util.Deref(c.GetTopic()), util.Deref(c.GetId())))
+		}
+		return "", fmt.Errorf("multiple chats with given topic %q found: \n%s. \nPlease use one of the IDs to resolve the chat",
+			topic, strings.Join(options, ";\n"))
+	}
 }
