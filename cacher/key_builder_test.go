9da8709ddd6abb8bff6611b00c6a0e10b3497a8e
Michał Szwejk <mszwejk1@gmail.com>
2025-12-29 12:52:52 +0100

    test: add tests for cache  key_builder

--- cacher/key_builder_test.go
+++ cacher/key_builder_test.go
@@ -24,11 +24,42 @@ func TestNewChannelKey(t *testing.T) {
 	}
 }
 
-func TestNewMemberKey(t *testing.T) {
+func TestNewOneOnOneChatKey(t *testing.T) {
 	testPepper := "test-pepper"
-	got := NewChannelMemberKey("user@example.com", "team-123", "chan-456", &testPepper)
+	got := NewOneOnOneChatKey("user@example.com", &testPepper)
 	hashedEmail := util.HashWithPepper(testPepper, "user@example.com")
-	want := "$member$:team-123:chan-456:" + hashedEmail
+	want := "$direct-chat$:" + hashedEmail
+
+	if got != want {
+		t.Fatalf("OneOnOneChatKeyBuilder.ToString() = %q, want %q", got, want)
+	}
+}
+
+func TestNewGroupChatKey(t *testing.T) {
+	got := NewGroupChatKey("Project Alpha")
+	want := "$group-chat$:Project Alpha"
+
+	if got != want {
+		t.Fatalf("GroupChatKeyBuilder.ToString() = %q, want %q", got, want)
+	}
+}
+
+func TestNewGroupChatMemberKey(t *testing.T) {
+	testPepper := "test-pepper"
+	got := NewGroupChatMemberKey("chat-123", "user@example.com", &testPepper)
+	hashedEmail := util.HashWithPepper(testPepper, "user@example.com")
+	want := "$group-chat-member$:chat-123:" + hashedEmail
+
+	if got != want {
+		t.Fatalf("GroupChatMemberKeyBuilder.ToString() = %q, want %q", got, want)
+	}
+}
+
+func TestNewTeamMemberKey(t *testing.T) {
+	testPepper := "test-pepper"
+	got := NewChannelMemberKey("team-123", "chan-456", "user@example.com", &testPepper)
+	hashedEmail := util.HashWithPepper(testPepper, "user@example.com")
+	want := "$channel-member$:team-123:chan-456:" + hashedEmail
 
 	if got != want {
 		t.Fatalf("MemberKeyBuilder.ToString() = %q, want %q", got, want)
