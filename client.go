889afed3842d62d697f368bc29ac441ce9fc078c
Michał Szwejk <mszwejk1@gmail.com>
2025-12-31 21:05:00 +0100

    refactor: simplify client

--- client.go
+++ client.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 
 	graph "github.com/microsoftgraph/msgraph-sdk-go"
-
+	"github.com/pzsp-teams/lib/channels"
 	"github.com/pzsp-teams/lib/chats"
+	"github.com/pzsp-teams/lib/config"
 	"github.com/pzsp-teams/lib/internal/api"
 	"github.com/pzsp-teams/lib/internal/auth"
 	"github.com/pzsp-teams/lib/internal/cacher"
 	"github.com/pzsp-teams/lib/internal/resolver"
-
-	"github.com/pzsp-teams/lib/channels"
 	"github.com/pzsp-teams/lib/teams"
 )
 
@@ -20,66 +19,54 @@ type Client struct {
 	Channels channels.Service
 	Teams    teams.Service
 	Chats    chats.Service
-	waitFns  []func()
 }
 
 // NewClient will be used later
-func NewClient(ctx context.Context, authConfig *AuthConfig, senderConfig *SenderConfig, cacheEnabled bool, cachePath *string) (*Client, error) {
-	tokenProvider, err := auth.NewMSALTokenProvider(authConfig.toMSALCredentials())
+func NewClient(ctx context.Context, authCfg *config.AuthConfig, senderCfg *config.SenderConfig, cacheCfg *config.CacheConfig) (*Client, error) {
+	tokenProvider, err := auth.NewMSALTokenProvider(authCfg)
 	if err != nil {
 		return nil, fmt.Errorf("creating token provider: %w", err)
 	}
 
-	graphClient, err := graph.NewGraphServiceClientWithCredentials(tokenProvider, authConfig.Scopes)
+	graphClient, err := graph.NewGraphServiceClientWithCredentials(tokenProvider, authCfg.Scopes)
 	if err != nil {
 		return nil, fmt.Errorf("creating graph client: %w", err)
 	}
 
-	return NewClientFromGraphClient(graphClient, senderConfig, cacheEnabled, cachePath)
-}
+	teamsAPI := api.NewTeams(graphClient, senderCfg)
+	channelsAPI := api.NewChannels(graphClient, senderCfg)
+	chatAPI := api.NewChat(graphClient, senderCfg)
+	userAPI := api.NewUsers(graphClient, senderCfg)
 
-func NewClientFromGraphClient(graphClient *graph.GraphServiceClient, senderConfig *SenderConfig, cacheEnabled bool, cachePath *string) (*Client, error) {
-	var cache cacher.Cacher
-	if cacheEnabled && cachePath != nil {
-		cache = cacher.NewJSONFileCacher(*cachePath)
-	} else if cacheEnabled {
-		cache = cacher.NewJSONFileCacher(defaultCachePath())
-	}
-	techParams := senderConfig.toTechParams()
+	cacheHandler := cacher.NewCacheHandler(cacheCfg)
 
-	teamsAPI := api.NewTeams(graphClient, techParams)
-	channelsAPI := api.NewChannels(graphClient, techParams)
-	chatAPI := api.NewChat(graphClient, techParams)
-	userAPI := api.NewUsers(graphClient, techParams)
-
-	teamResolver := resolver.NewTeamResolverCacheable(teamsAPI, cache, cacheEnabled)
-	channelResolver := resolver.NewChannelResolverCacheable(channelsAPI, cache, cacheEnabled)
-	chatResolver := resolver.NewChatResolverCacheable(chatAPI, cache, cacheEnabled)
+	// TODO: make resolvers with cache decorators, the same as services
+	teamResolver := resolver.NewTeamResolverCacheable(teamsAPI, cacheHandler)
+	channelResolver := resolver.NewChannelResolverCacheable(channelsAPI, cacheHandler)
+	chatResolver := resolver.NewChatResolverCacheable(chatAPI, cacheHandler)
 
 	teamSvc := teams.NewService(teamsAPI, teamResolver)
 	channelSvc := channels.NewService(channelsAPI, teamResolver, channelResolver, userAPI)
 	chatSvc := chats.NewService(chatAPI, chatResolver, userAPI)
-	waitFns := make([]func(), 0, 2)
-	if cacheEnabled {
-		teamSvc = teams.NewAsyncServiceWithCache(teamSvc, cache)
-		channelSvc = channels.NewAsyncServiceWithCache(channelSvc, cache, teamResolver, channelResolver)
-		if w, ok := channelSvc.(interface{ Wait() }); ok {
-			waitFns = append(waitFns, w.Wait)
-		}
-		if w, ok := teamSvc.(interface{ Wait() }); ok {
-			waitFns = append(waitFns, w.Wait)
-		}
+
+	if cacheHandler != nil {
+		channelSvc = channels.NewServiceWithCache(channelSvc, teamResolver, channelResolver, cacheHandler)
+		teamSvc = teams.NewServiceWithCache(teamSvc, cacheHandler)
 	}
+
 	return &Client{
 		Channels: channelSvc,
 		Teams:    teamSvc,
 		Chats:    chatSvc,
-		waitFns:  waitFns,
 	}, nil
 }
 
 func (c *Client) Close() {
-	for _, fn := range c.waitFns {
-		fn()
+	if w, ok := c.Channels.(interface{ Wait() }); ok {
+		w.Wait()
+	}
+
+	if w, ok := c.Teams.(interface{ Wait() }); ok {
+		w.Wait()
 	}
 }
