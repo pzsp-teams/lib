3916042c0ce9d5b91f39a9fde60ba2b3bee620a3
KamilMarszalek <kamilmar553@gmail.com>
2025-12-31 09:24:59 +0100

    docs: update documentation for services

--- chats/service_interface.go
+++ chats/service_interface.go
@@ -6,6 +6,7 @@
 //   - Users are identified by userID or email.
 //   - Some operations require messageID - these can be obtained via ListMessages.
 //   - ChatRef and GroupChatRef are references to chats used in method parameters.
+//   - If chatRef is a topic and is not unique, an ambiguity error is returned.
 //   - The authenticated user (derived from MSAL) is the one making the API calls (appropriate scopes must be granted).
 package chats
 
@@ -42,7 +43,8 @@ type Service interface {
 	// ListMessages returns all messages in a chat.
 	ListMessages(ctx context.Context, chatRef ChatRef) ([]*models.Message, error)
 
-	// SendMessage sends a message to a chat. Body parameter is the body of the message. It includes:
+	// SendMessage sends a message to a chat. 
+	// Body parameter is the body of the message. It includes:
 	//   - Content: the text or html content of the message.
 	//   - ContentType: the type of content (text or html).
 	//   - Mentions: optional mentions to include in the message.
