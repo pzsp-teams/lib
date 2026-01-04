9d0b87d42cdf87856705a3a2d6c3893584976493
KamilMarszalek <kamilmar553@gmail.com>
2026-01-04 22:55:19 +0100

    refactor: make initializing client or services independent

--- client.go
+++ client.go
@@ -21,33 +21,45 @@ type Client struct {
 	Chats    chats.Service
 }
 
+var graphClient *graph.GraphServiceClient
+
+func getGraphClient(authCfg *config.AuthConfig) (*graph.GraphServiceClient, error) {
+	if graphClient == nil {
+		tokenProvider, err := auth.GetMSALTokenProvider(authCfg)
+		if err != nil {
+			return nil, err
+		}
+
+		graphClient, err = graph.NewGraphServiceClientWithCredentials(tokenProvider, authCfg.Scopes)
+		if err != nil {
+			return nil, fmt.Errorf("creating graph client: %w", err)
+		}
+	}
+	return graphClient, nil
+}
+
 // NewClient will be used later
 func NewClient(ctx context.Context, authCfg *config.AuthConfig, senderCfg *config.SenderConfig, cacheCfg *config.CacheConfig) (*Client, error) {
-	tokenProvider, err := auth.NewMSALTokenProvider(authCfg)
+	cl, err := getGraphClient(authCfg)
 	if err != nil {
-		return nil, fmt.Errorf("creating token provider: %w", err)
+		return nil, err
 	}
 
-	graphClient, err := graph.NewGraphServiceClientWithCredentials(tokenProvider, authCfg.Scopes)
-	if err != nil {
-		return nil, fmt.Errorf("creating graph client: %w", err)
-	}
-
-	return NewClientFromGraphClient(graphClient, senderCfg, cacheCfg)
+	return NewClientFromGraphClient(cl, senderCfg, cacheCfg)
 }
 
 func NewClientFromGraphClient(graphClient *graph.GraphServiceClient, senderCfg *config.SenderConfig, cacheCfg *config.CacheConfig) (*Client, error) {
-	teamsAPI := api.NewTeams(graphClient, senderCfg)
-	channelAPI := api.NewChannels(graphClient, senderCfg)
-	chatAPI := api.NewChat(graphClient, senderCfg)
-	userAPI := api.NewUser(graphClient, senderCfg)
+	teamsAPI := api.GetTeamAPI(graphClient, senderCfg)
+	channelAPI := api.GetChannelAPI(graphClient, senderCfg)
+	chatAPI := api.GetChatAPI(graphClient, senderCfg)
+	userAPI := api.GetUserAPI(graphClient, senderCfg)
 
-	cacheHandler := cacher.NewCacheHandler(cacheCfg)
+	cacheHandler := cacher.GetCacheHandler(cacheCfg)
 
 	// TODO: make resolvers with cache decorators, the same as services
-	teamResolver := resolver.NewTeamResolverCacheable(teamsAPI, cacheHandler)
-	channelResolver := resolver.NewChannelResolverCacheable(channelAPI, cacheHandler)
-	chatResolver := resolver.NewChatResolverCacheable(chatAPI, cacheHandler)
+	teamResolver := resolver.GetTeamResolver(teamsAPI, cacheHandler)
+	channelResolver := resolver.GetChannelResolver(channelAPI, cacheHandler)
+	chatResolver := resolver.GetChatResolver(chatAPI, cacheHandler)
 
 	channelOps := channels.NewOps(channelAPI, userAPI)
 	teamOps := teams.NewOps(teamsAPI)
@@ -69,12 +81,70 @@ func NewClientFromGraphClient(graphClient *graph.GraphServiceClient, senderCfg *
 	}, nil
 }
 
-func (c *Client) Close() {
-	if w, ok := c.Channels.(interface{ Wait() }); ok {
-		w.Wait()
+func NewChannelServiceFromGraphClient(ctx context.Context, authCfg *config.AuthConfig, senderCfg *config.SenderConfig, cacheCfg *config.CacheConfig) (channels.Service, error) {
+	cl, err := getGraphClient(authCfg)
+	if err != nil {
+		return nil, err
 	}
+	channelAPI := api.GetChannelAPI(cl, senderCfg)
+	userAPI := api.GetUserAPI(cl, senderCfg)
+	teamAPI := api.GetTeamAPI(cl, senderCfg)
 
-	if w, ok := c.Teams.(interface{ Wait() }); ok {
-		w.Wait()
+	cacheHandler := cacher.GetCacheHandler(cacheCfg)
+	teamResolver := resolver.GetTeamResolver(teamAPI, cacheHandler)
+	channelResolver := resolver.GetChannelResolver(channelAPI, cacheHandler)
+
+	channelOps := channels.NewOps(channelAPI, userAPI)
+	if cacheHandler != nil {
+		channelOps = channels.NewOpsWithCache(channelOps, cacheHandler)
+	}
+	channelSvc := channels.NewService(channelOps, teamResolver, channelResolver)
+
+	return channelSvc, nil
+}
+
+func NewTeamServiceFromGraphClient(ctx context.Context, authCfg *config.AuthConfig, senderCfg *config.SenderConfig, cacheCfg *config.CacheConfig) (teams.Service, error) {
+	cl, err := getGraphClient(authCfg)
+	if err != nil {
+		return nil, err
+	}
+	teamAPI := api.GetTeamAPI(cl, senderCfg)
+
+	cacheHandler := cacher.GetCacheHandler(cacheCfg)
+	teamResolver := resolver.GetTeamResolver(teamAPI, cacheHandler)
+
+	teamOps := teams.NewOps(teamAPI)
+	if cacheHandler != nil {
+		teamOps = teams.NewOpsWithCache(teamOps, cacheHandler)
+	}
+	teamSvc := teams.NewService(teamOps, teamResolver)
+
+	return teamSvc, nil
+}
+
+func NewChatServiceFromGraphClient(ctx context.Context, authCfg *config.AuthConfig, senderCfg *config.SenderConfig, cacheCfg *config.CacheConfig) (chats.Service, error) {
+	cl, err := getGraphClient(authCfg)
+	if err != nil {
+		return nil, err
+	}
+	chatAPI := api.GetChatAPI(cl, senderCfg)
+	userAPI := api.GetUserAPI(cl, senderCfg)
+
+	cacheHandler := cacher.GetCacheHandler(cacheCfg)
+	chatResolver := resolver.GetChatResolver(chatAPI, cacheHandler)
+
+	chatOps := chats.NewOps(chatAPI, userAPI)
+	if cacheHandler != nil {
+		chatOps = chats.NewOpsWithCache(chatOps, cacheHandler)
+	}
+	chatSvc := chats.NewService(chatOps, chatResolver)
+
+	return chatSvc, nil
+}
+
+// Close waits for all background operations to complete.
+func Close() {
+	if cacher.Singleton != nil {
+		cacher.Singleton.Runner.Wait()
 	}
 }
