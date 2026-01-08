7a8836d07a18fc2ead67a0c13886142865d9eabd
Michal <01187288@pw.edu.pl>
2026-01-08 13:43:15 +0100

    docs: generated markdown docs and used mkdocs for website view

---
+++ docs/generate_docs.sh
@@ -0,0 +1,6 @@
+gomarkdoc ./teams --output docs/teams.md
+gomarkdoc ./channels --output docs/channels.md
+gomarkdoc ./chats --output docs/chats.md
+gomarkdoc ./models --output docs/models.md
+gomarkdoc ./config --output docs/config.md
+gomarkdoc . --output docs/client.md
