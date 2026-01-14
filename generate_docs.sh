f22c94a80d5a37329048dd1dece11d9e75c6058f
Michal <01187288@pw.edu.pl>
2026-01-14 18:12:49 +0100

    Deployed 3554d61 with MkDocs version: 1.6.1

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,6 +1,7 @@
-gomarkdoc ./teams --output docs/teams.md
-gomarkdoc ./channels --output docs/channels.md
-gomarkdoc ./chats --output docs/chats.md
-gomarkdoc ./models --output docs/models.md
-gomarkdoc ./config --output docs/config.md
-gomarkdoc . --output docs/client.md
+$(go env GOPATH)/bin/gomarkdoc ./teams --output docs/teams.md
+$(go env GOPATH)/bin/gomarkdoc ./channels --output docs/channels.md
+$(go env GOPATH)/bin/gomarkdoc ./chats --output docs/chats.md
+$(go env GOPATH)/bin/gomarkdoc ./models --output docs/models.md
+$(go env GOPATH)/bin/gomarkdoc ./config --output docs/config.md
+$(go env GOPATH)/bin/gomarkdoc ./search --output docs/search.md
+$(go env GOPATH)/bin/gomarkdoc . --output docs/client.md
